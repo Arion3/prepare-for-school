c20a7bc7dc776a8f79f819dfee1e7a946a3b107e
Arion3 <artyomshishov65@gmail.com>
2026-02-06 13:34:40 +0300

    sum3

--- helloworld.c
+++ helloworld.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 int main(void)
 {
-    int a, b, res;
-    scanf("%d", &a);
-    scanf("%d", &b);
+    int a = 0, b = 0, res;
+
+    scanf("%d", &a); // считываем целое значение в переменную a
+    scanf("%d", &b); // считываем целое значение в переменную b
+
     res = a + b;
     printf("%d + %d = %d\n", a, b, res);
-    return (0);
+
+    return 0;
 }
